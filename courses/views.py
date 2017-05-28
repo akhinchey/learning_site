7ed6dfd7532f774d8dd6a3a2fcd628e70ad30f5f
Amanda Hinchey <akhinchey@gmail.com>
2017-05-28 18:10:21 -0400

    Add basic templates for course list and homepage.

--- courses/views.py
+++ courses/views.py
@@ -9,5 +9,4 @@ from .models import Course
 # Create your views here.
 def all_courses(request):
   courses = Course.objects.all()
-  output = ', '.join([str(course) for course in courses])
-  return HttpResponse(output)
+  return render(request, 'courses/all_courses.html', { 'courses': courses })
